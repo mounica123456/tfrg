4765fb2ed4e20f7b235b67670f5c099634fb0117
mounica123456 <mounicasathwik@outlook.com>
2020-10-08 18:03:35 +0530

    main

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
 
-  version         = "=2.20.0"
+  version         = "=2.4.0"
   subscription_id = "88b7a74c-47d4-48ed-b11f-9bac8f99264c"
   client_id       = "4fe13f05-87c0-4e6e-8e33-3df885a7704e"
   client_secret   = "4d_LfSPBooQdNN~6D8.ysrNpv3zWs-kXP0"
