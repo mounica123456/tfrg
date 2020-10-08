33a45af554a44c84b41f3f034fdddee8dd622852
mounica123456 <mounicasathwik@outlook.com>
2020-10-08 18:43:12 +0530

    changed provider

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   version         = "=2.4.0"
   subscription_id = "88b7a74c-47d4-48ed-b11f-9bac8f99264c"
   client_id       = "4fe13f05-87c0-4e6e-8e33-3df885a7704e"
-  client_secret   = "4d_LfSPBooQdNN~6D8.ysrNpv3zWs-kXP0"
+  client_secret   = "699.r-38AKygLigVNVQ6_GSlK6a62pm-W~"
   tenant_id       = "70973106-6728-471c-8ec4-08f6fa9e70bd"
   features {}
 }
