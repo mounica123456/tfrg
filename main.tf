f79dd19964bed2b00461b543b2ab30435a6ff3dc
mounica123456 <mounicasathwik@outlook.com>
2020-10-09 11:32:56 +0530

    commit

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "azurerm" {
 
   version         = "=2.4.0"
   subscription_id = "88b7a74c-47d4-48ed-b11f-9bac8f99264c"
-  client_id       = "4fe13f05-87c0-4e6e-8e33-3df885a7704e"
-  client_secret   = "699.r-38AKygLigVNVQ6_GSlK6a62pm-W~"
+  client_id       = "3afdb773-6870-47e8-b5bc-1d2145052009"
+  client_secret   = "nhE4rM8INe108V8nt_my9Z98v-qkOU~H0."
   tenant_id       = "70973106-6728-471c-8ec4-08f6fa9e70bd"
   features {}
 }
